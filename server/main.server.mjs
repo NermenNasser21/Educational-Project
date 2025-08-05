ccec073d30ddfeadebddf95a59c6d45e02834847
NermenNasser21 <nermen.nasser218@gmail.com>
2025-08-05 16:28:33 +0300

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-J2X47KZM.mjs";import"./chunk-JLOHZMVY.mjs";import"./chunk-ELXMVXQU.mjs";import"./chunk-LTKC4KDD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
