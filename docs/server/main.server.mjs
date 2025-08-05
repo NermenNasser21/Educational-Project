c33ad7b80482736e5071800e530fcef11f33cee3
NermenNasser21 <nermen.nasser218@gmail.com>
2025-08-05 15:36:49 +0300

    build

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UDZ7JQXS.mjs";import"./chunk-AQ4DAAWU.mjs";import"./chunk-ZB3VVLU4.mjs";import"./chunk-EPTXVPS7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
